72eb8cec6cdee1c1f5d63bfe80580a0f68a21df2
Jason Doar <jbdoar@gmail.com>
2022-07-28 02:45:49 -0700

    adding sitemap thingy

--- render.py
+++ render.py
@@ -5,12 +5,16 @@ import sys
 import markdown
 
 def convert_imtag(text):
+    """
+    replace markdown-type embedded image tag with html / automatic sizing
+    """
     for md in re.findall('\!.*?\)', text):
         alt = md[md.find('[')+1 : md.find(']')]
         url = md[md.find('https://') : md.find("\"")]
         html = f"<img src=\"{url}\" alt=\"{alt}\" title=\"{alt}\" width=100% height=auto>"
         text = text.replace(md, html)
     return text
+
 
 # clear the docs directory
 cwd = os.getcwd()
@@ -19,14 +23,21 @@ for file in os.listdir():
     os.remove(file)
 os.chdir(cwd)
 
+
 # load html style stuff
 with open('template.html', 'r') as f:
     template = f.read()
 
-# loop through the markdown directory
-filenames = os.listdir('markdown')
 
-for file in filenames:
+# make sitemap
+with open(os.path.join('markdown', 'sitemap.md'), 'w') as sitemap:
+    for file in os.listdir('markdown'):
+        page = file.replace(".md", "")
+        sitemap.write(f"[{page}]({page}.html)\r")
+
+
+# loop through the markdown directory
+for file in os.listdir('markdown'):
     # read the markdown file and convert to html
     with open(os.path.join('markdown', file), 'r') as f:
         text = f.read()
